a070882be9567b3332a2c2a92c69f2c19b7235d9
Eric <eric.earlywine@hotmail.com>
2019-11-20 20:24:51 -0600

    Finished lesson 5 exercise

--- src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
+++ src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
@@ -47,9 +47,11 @@ public class Car {
     }
 
     public void setGasTankLevel(double gasTankLevel) {
+        if (gasTankLevel > this.getGasTankSize()) {
+            throw new IllegalArgumentException("Can't exceed tank size");
+        }
         this.gasTankLevel = gasTankLevel;
     }
-
     public double getMilesPerGallon() {
         return milesPerGallon;
     }
@@ -84,5 +86,7 @@ public class Car {
         this.gasTankLevel = this.gasTankLevel - gallonsUsed;
         this.odometer += milesAbleToTravel;
     }
-
+    public void addGas(double gas) {
+        this.setGasTankLevel(gas + this.getGasTankLevel());
+    }
 }
